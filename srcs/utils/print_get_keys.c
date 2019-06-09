8a7b0c7d7a7950be76e4c9a9fc519cfbcf6e3a71
Pcorlys- <Yarosleu@gmail.com>
2019-06-09 22:17:58 +0300

    finish - norminette

--- srcs/utils/print_get_keys.c
+++ srcs/utils/print_get_keys.c
@@ -6,17 +6,22 @@
 /*   By: galiza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 17:56:29 by galiza            #+#    #+#             */
-/*   Updated: 2019/06/09 21:29:10 by pcorlys-         ###   ########.fr       */
+/*   Updated: 2019/06/09 22:13:06 by pcorlys-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_get_keys(const char *fmt, int curr_chr, t_flags *flags)
+static void	if_dot(t_flags *flags, int curr_chr, int i, const char *fmt)
 {
-	int	i;
+	(*flags).t_dot = ft_atoi(fmt + curr_chr + i + 1);
+	if ((*flags).t_dot <= 0)
+		(*flags).t_dot = -1;
+	(*flags).dot = 1;
+}
 
-	i = 0;
+static int	stan_param(t_flags *flags)
+{
 	(*flags).minus = 0;
 	(*flags).dot = 0;
 	(*flags).blank = 0;
@@ -27,23 +32,38 @@ void	ft_get_keys(const char *fmt, int curr_chr, t_flags *flags)
 	(*flags).h_tag = 0;
 	(*flags).total = 0;
 	(*flags).un_tot = 0;
+	return (0);
+}
+
+static void	if_other(t_flags *flags, int curr_chr, const char *fmt)
+{
+	int		q;
+
+	q = 0;
+	while (ft_strchr("#-+*.hl 0123456789", fmt[curr_chr + q]))
+	{
+		if (fmt[curr_chr + q] == '-')
+			(*flags).minus = 1;
+		if (fmt[curr_chr + q] == '.')
+			if_dot(flags, curr_chr, q, fmt);
+		if (fmt[curr_chr + q] == '#')
+			(*flags).h_tag = 1;
+		if (fmt[curr_chr + q] == ' ')
+			(*flags).blank = 1;
+		if (fmt[curr_chr + q] == '+')
+			(*flags).plus = 1;
+		q++;
+	}
+}
+
+void		ft_get_keys(const char *fmt, int curr_chr, t_flags *flags)
+{
+	int		i;
+
+	i = stan_param(flags);
+	if_other(flags, curr_chr, fmt);
 	while (ft_strchr("#-+*.hl 0123456789", fmt[curr_chr + i]))
 	{
-		if (fmt[curr_chr + i] == '-')
-			(*flags).minus = 1;
-		if (fmt[curr_chr + i] == '.')
-		{
-			(*flags).t_dot = ft_atoi(fmt + curr_chr + i + 1);
-			if ((*flags).t_dot <= 0)
-				(*flags).t_dot = -1;
-			(*flags).dot = 1;
-		}
-		if (fmt[curr_chr + i] == '#')
-			(*flags).h_tag = 1;
-		if (fmt[curr_chr + i] == ' ')
-			(*flags).blank = 1;
-		if (fmt[curr_chr + i] == '+')
-			(*flags).plus = 1;
 		if (fmt[curr_chr + i] == 'h' && fmt[curr_chr + i + 1] == 'h')
 			(*flags).flags |= HH;
 		if (fmt[curr_chr + i] == 'h')
